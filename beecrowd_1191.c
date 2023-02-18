776c61342e9a49acb7e8b5752c170e7bfa3d1959
veqis <alyssonhenrique000@gmail.com>
2023-02-18 19:29:43 -0300

    questão nova

---
+++ beecrowd_1191.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct arv{
+  char info;
+  struct arv *dir;
+  struct arv *esq;
+}Arv;
+
+int main()
+{
+    
+    return 0;
+}
