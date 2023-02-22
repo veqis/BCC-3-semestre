382b3993eda045cc282e2730b3e30f5b6bf6bce6
veqis <alyssonhenrique000@gmail.com>
2023-02-22 11:14:56 -0300

    {

--- arvore.c
+++ arvore.c
@@ -1,30 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "arvores.h"
+#include <stdbool.h>
 
-int main(){
-  int opc;
-  int val;
-  int curr=0, runs;
+typedef struct arv {
+    int info;
+    int nivel;
+    struct arv* dir;
+    struct arv* esq;
+} Arv;
 
-  scanf("%d",&runs);
+Arv* insert(int c, int niv, Arv* raiz) {
+    if (raiz == NULL) {
+        Arv* novo = (Arv*)malloc(sizeof(Arv));
+        novo->info = c;
+        novo->nivel = niv;
+        novo->dir = novo->esq = NULL;
+        return novo;
+    } else if (c < raiz->info) {
+        raiz->esq = insert(c, niv + 1, raiz->esq);
+    } else {
+        raiz->dir = insert(c, niv + 1, raiz->dir);
+    }
+    return raiz;
+}
 
-  Arv*a=NULL;
-  while (curr!=runs) {
-    printf ("(1)inserir (2)remover (3)imprimir (0) sair --> ");
-    scanf("%d",&opc );
+void impr_nivel(Arv* raiz, int nivel_atual) {
+    if (raiz == NULL) {
+        return;
+    }
 
-        printf("Caractere: ");
-        scanf(" %d",&val);
-        a=ins_abb(val,a);
-    curr++;
-  }
-    system ("clear");
-    printf("\npre: "); impr_pre(a);
-    printf("\nord: "); impr_ord(a);
-    printf("\npos: "); impr_pos(a);
-    printf ("\n");
+    if (raiz->nivel == nivel_atual) {
+        printf("%d ", raiz->info);
+    }
 
-  printf("\n");
-  return 0;
+    impr_nivel(raiz->esq, nivel_atual + 1);
+    impr_nivel(raiz->dir, nivel_atual + 1);
+}
+
+int main() {
+    Arv* root = NULL;
+    int runs, qtd, in;
+
+    scanf("%d", &runs);
+    for (int i = 0; i < runs; i++) {
+        root = NULL;
+
+        scanf("%d", &qtd);
+        for (int b = 0; b < qtd; b++) {
+            scanf("%d", &in);
+            root = insert(in, 0, root);
+        }
+
+        for (int nivel = 0; nivel < qtd; nivel++) {
+            impr_nivel(root, nivel);
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
